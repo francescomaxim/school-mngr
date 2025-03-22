f97d97890ff71ec3947e997cfbe8059e2e17bfb7
francesco.maxim <francesco.maxim@icloud.com>
2025-03-22 22:53:26 +0200

    added header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './features/auth/login/login.component';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   {
     path: '**',
     component: AppComponent,
