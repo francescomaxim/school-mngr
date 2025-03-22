e410e9736bb387fc89f9d81f7e041fe8f389c9b6
francesco.maxim <francesco.maxim@icloud.com>
2025-03-23 01:46:34 +0200

    added guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { HeroComponent } from './shared/components/hero/hero.component';
 import { AdminPanelComponent } from './features/admin/admin-panel/admin-panel.component';
+import { adminGuard } from './core/authentication/guards/admin.guard';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,7 @@ export const routes: Routes = [
   },
   {
     path: 'adminpanel',
+    canActivate: [adminGuard],
     component: AdminPanelComponent,
   },
   {
---
+++ src/app/core/authentication/guards/admin.guard.ts
@@ -0,0 +1,26 @@
+import { inject, Injectable } from '@angular/core';
+import {
+  CanActivateFn,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthService } from '../auth.service';
+
+export const adminGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const user = authService.user.getValue();
+
+  if (user && user.role === 'admin') {
+    return true;
+  }
+
+  // Redirect dacă nu e admin
+  router.navigate(['/login']);
+  return false;
+};
